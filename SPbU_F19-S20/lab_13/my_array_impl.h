8c4cfb9c1df199c6e51c8cea1a57e7d4d36219eb
Vyacheslav Tamarin <slavabarsuk@ya.ru>
2020-03-15 14:43:03 +0300

    go branch for not specification version

--- SPbU_F19-S20/lab_13/my_array_impl.h
+++ SPbU_F19-S20/lab_13/my_array_impl.h
@@ -50,44 +50,4 @@ std::size_t my_array<T, N>::size() const  {
     return N;
 }
 
-
-template<std::size_t N>
-my_array<bool, N>::my_array() {
-    // std::cout << (N / 8 + (1 ? N % 8 != 0 : 0)) << std::endl;
-    // // array_ = new uint8_t[N / 8 + (1 ? N % 8 != 0 : 0)];
-    // // std::cout << sizeof(a) << std::endl;
-    // // array_ = a;
-    // std::cout << sizeof(array_) << std::endl;
-    // std::cout << sizeof(this->array_) << std::endl;
-}
-
-template<std::size_t N>
-my_array<bool, N>::~my_array() {
-    // delete [] array_;
-}
-
-template<std::size_t N>
-bool my_array<bool, N>::empty() const  {
-    return size() == 0;
-}
-
-template<std::size_t N>
-void my_array<bool, N>::fill(bool val) {
-    if (val)
-	for (std::size_t i=0; i<size(); ++i) {
-	    array_[i] = (1 << 8) - 1;
-	}
-    else {
-	for (std::size_t i=0; i<size(); ++i) {
-	    array_[i] = 0;
-	}
-    }
-}
-
-template<std::size_t N>
-std::size_t my_array<bool, N>::size() const  {
-    return N;
-}
-
-
 #endif  // MY_ARRAY_IMPL_H_
